ccd215739dffa08bf86067e59c4fc970058c56dc
rapha <raphael@dichler.com>
2022-06-28 12:19:19 +0200

    Initial commit

---
+++ src/main/java/net/plyse/api/google/places/Main.java
@@ -0,0 +1,12 @@
+package net.plyse.api.google.places;
+
+/**
+ * @author Raphael Dichler on 28.06.2022.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
