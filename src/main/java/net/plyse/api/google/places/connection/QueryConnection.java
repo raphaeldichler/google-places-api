2fec8eb53ab2c5c0431447b3f3a1223148de5dc0
Raphael Dichler <raphael.dichler@tum.de>
2022-07-08 16:00:42 +0200

    restructure search

--- src/main/java/net/plyse/api/google/places/connection/QueryConnection.java
+++
@@ -1,7 +0,0 @@
-package net.plyse.api.google.places.connection;
-
-public interface QueryConnection {
-
-    void execute(String query);
-
-}
--- src/main/java/net/plyse/api/google/places/connection/Url.java
+++
@@ -1,9 +0,0 @@
-package net.plyse.api.google.places.connection;
-
-import net.plyse.api.google.places.query.parameter.Parameter;
-
-public interface Url extends ChangeableUrl {
-
-    String getUrl();
-
-}
