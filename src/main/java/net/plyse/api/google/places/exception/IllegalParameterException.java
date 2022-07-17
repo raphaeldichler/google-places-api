f3a729852ab5c7fae8795d971552cb2bd6195015
Raphael Dichler <44930718+AuTEraZer@users.noreply.github.com>
2022-07-17 22:37:07 +0200

    added Exceptions, which are thrown when the library is not used properly

---
+++ src/main/java/net/plyse/api/google/places/exception/IllegalParameterException.java
@@ -0,0 +1,17 @@
+package net.plyse.api.google.places.exception;
+
+/**
+ * Thrown to indicate a parameter is not valid for a certain request.
+ *
+ * @author Raphael Dichler on 17.07.2022.
+ */
+public class IllegalParameterException extends RuntimeException {
+
+    public IllegalParameterException() {
+    }
+
+    public IllegalParameterException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/net/plyse/api/google/places/filter/Filter.java
+++ src/main/java/net/plyse/api/google/places/filter/Filter.java
@@ -1,4 +1,6 @@
 package net.plyse.api.google.places.filter;
 
 public class Filter {
+
+
 }
