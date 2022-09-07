d5ccdd5dbe8df79267bbc4777184287270672029
Raphael Dichler <raphael@dichler.com>
2022-09-07 17:24:24 +0200

    added TextSearch test and fixed bug in TextSearchRequest (missing & after query)

--- src/main/java/net/plyse/google/api/place/model/TextSearchRequest.java
+++ src/main/java/net/plyse/google/api/place/model/TextSearchRequest.java
@@ -1,14 +1,76 @@
 package net.plyse.google.api.place.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.plyse.google.api.place.exception.IllegalParameterException;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@JsonInclude(Include.NON_NULL)
 public class TextSearchRequest implements Request {
 
     private static final String OUTPUT = "json";
 
-    private static final String BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/" + OUTPUT;
+    private static final String BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/" + OUTPUT ;
+    private String query;
+    private String language;
+    private LatLngLiteral location;
+    private int maxPrice = -1;
+    private int minPrice = -1;
+    private Boolean openNow;
+    private String pageToken;
+    private int radius = -1;
+    private String region;
+    private String type;
+
+    public static void main(String[] args) throws JsonProcessingException {
+        String jsonString =
+                "{\n" +
+                "\t\"query\": \"Hello World\",\n" +
+                "\t\"language\": \"de\",\n" +
+                "\t\"location\": {\n" +
+                "\t\t\"lat\": \"13.432\",\n" +
+                "\t\t\"lng\": \"-78.928\"\n" +
+                "\t}\n" +
+                "}";
+        ObjectMapper mapper = new ObjectMapper();
+        TextSearchRequest textSearchRequest = mapper.readValue(jsonString, TextSearchRequest.class);
+
+
+        System.out.println(textSearchRequest);
+    }
 
     @Override
     public String getUrl() {
-        return null;
+        if (query == null || query.isEmpty()) {
+            throw new IllegalParameterException();
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(BASE_URL).append("?query=").append(query).append("&");
+
+        if (language != null) stringBuilder.append("language=").append(language).append("&");
+        if (location != null) stringBuilder.append("location=").append(location.getLat())
+                    .append("%2C").append(location.getLng())
+                    .append("&");
+        if (maxPrice != -1) stringBuilder.append("maxprice=").append(maxPrice).append("&");
+        if (minPrice != -1) stringBuilder.append("minprice=").append(minPrice).append("&");
+        if (openNow != null) stringBuilder.append("opennow=").append(openNow).append("&");
+        if (pageToken != null) stringBuilder.append("pagetoken=").append(pageToken).append("&");
+        if (radius != -1) stringBuilder.append("radius=").append(radius).append("&");
+        if (region != null) stringBuilder.append("region=").append(region).append("&");
+        if (type != null) stringBuilder.append("type=").append(type).append("&");
+
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+
+        return stringBuilder.toString();
     }
+
+
 }
---
+++ src/main/test/net/plyse/google/api/place/model/RequestModelTest.java
@@ -0,0 +1,44 @@
+package net.plyse.google.api.place.model;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestModelTest {
+
+    private static final float LAT = (float) 2.2;
+    private static final float LNG = (float) 72.4;
+    private static final String TEXT_SEARCH_BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
+
+    static Stream<Arguments> textSearchTest() {
+        return Stream.of(
+            Arguments.of(new TextSearchRequest("query", "de", null, -1,
+                    -1, null, null, -1, null, null),
+                    TEXT_SEARCH_BASE_URL + "query=query&language=de"),
+                Arguments.of(new TextSearchRequest("query", null, new LatLngLiteral(LAT, LNG), -1,
+                                -1, null, null, -1, null, null),
+                        TEXT_SEARCH_BASE_URL + "query=query&location=" + LAT + "%2C" + LNG),
+                Arguments.of(new TextSearchRequest("query", null, null, 4,
+                                2, true, null, -1, null, null),
+                        TEXT_SEARCH_BASE_URL + "query=query&maxprice=4&minprice=2&opennow=true"),
+                Arguments.of(new TextSearchRequest("query", null, null, -1,
+                                -1 , null, "null", -1, null, null),
+                        TEXT_SEARCH_BASE_URL + "query=query&pagetoken=null")
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource({"textSearchTest"})
+    void urlTest(Request request, String expected) {
+        assertEquals(expected, request.getUrl());
+    }
+
+
+
+
+}
