7741e16905250f92c8b77a3f4948ab178606330a
Raphael Dichler <raphael@dichler.com>
2022-09-08 20:23:49 +0200

    impl Response for FindPlace

--- src/main/java/net/plyse/google/api/place/model/FindPlaceResponse.java
+++ src/main/java/net/plyse/google/api/place/model/FindPlaceResponse.java
@@ -1,4 +1,21 @@
 package net.plyse.google.api.place.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FindPlaceResponse implements Response {
+
+    private Place[] candidates;
+    private PlacesSearchStatus status;
+    private String error_message;
+    private String[] info_messages;
+
 }
