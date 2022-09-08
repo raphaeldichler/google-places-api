426de44b6c41b38a56487e484420c8ada7c56670
Raphael Dichler <raphael@dichler.com>
2022-09-08 20:18:57 +0200

    impl FindPlace

--- src/main/java/net/plyse/google/api/place/search/FindPlace.java
+++ src/main/java/net/plyse/google/api/place/search/FindPlace.java
@@ -1,15 +1,23 @@
 package net.plyse.google.api.place.search;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import lombok.AllArgsConstructor;
+import net.plyse.google.api.place.exchange.DataExchange;
 import net.plyse.google.api.place.model.FindPlaceResponse;
 import net.plyse.google.api.place.model.Request;
 
 import java.io.IOException;
-
+@AllArgsConstructor
 public class FindPlace implements Search<FindPlaceResponse> {
+
+    private final DataExchange dataExchange;
+    private final ObjectReader READER = new ObjectMapper().readerFor(FindPlaceResponse.class);
 
     @Override
     public FindPlaceResponse search(Request request) throws IOException {
-        return null;
+        String body = dataExchange.executeGetRequest(request.getUrl());
+        return READER.readValue(body, FindPlaceResponse.class);
     }
 
 }
