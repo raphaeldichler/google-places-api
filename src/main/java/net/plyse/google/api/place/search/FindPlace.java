7db0279fd7215da3d8a1951445f46e52d95257a1
Raphael Dichler <raphael@dichler.com>
2022-09-08 20:16:09 +0200

    impl FindPlace structure

---
+++ src/main/java/net/plyse/google/api/place/search/FindPlace.java
@@ -0,0 +1,15 @@
+package net.plyse.google.api.place.search;
+
+import net.plyse.google.api.place.model.FindPlaceResponse;
+import net.plyse.google.api.place.model.Request;
+
+import java.io.IOException;
+
+public class FindPlace implements Search<FindPlaceResponse> {
+
+    @Override
+    public FindPlaceResponse search(Request request) throws IOException {
+        return null;
+    }
+
+}
